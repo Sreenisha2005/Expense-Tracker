bb252417b2ffcbf95619ddc83831fb854d79fa38
sreenisha2005@gmail.com <sreenisha2005@gmail.com>
2025-10-30 17:46:05 +0530

    commit

--- src/main/java/com/expenseTracker/Expense/Tracker/Controller/AuthController.java
+++ src/main/java/com/expenseTracker/Expense/Tracker/Controller/AuthController.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/auth")
 public class AuthController {
     private final UserService userService;
